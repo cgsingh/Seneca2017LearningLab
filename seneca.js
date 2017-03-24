e629e6997084459b1bb5126f2ac8cf43c801bd7a
cgsingh <cgsingh@myseneca.ca>
2017-03-24 09:38:41 -0400

    first commit

---
+++ seneca.js
@@ -0,0 +1,26 @@
+// [INFO] See discussion of node.js exports here:
+// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+
+/**
+ * Given a string `email`, return `true` if the string is in the form
+ * of a valid Seneca College email address, `false` othewise.
+ */
+exports.isValidEmail = function(email) {
+	var containsAt = email.indexOf("@");
+	if (containsAt != -1 && containsAt != 0) //must exist, but must not be first char, obviously
+	{
+		if (email.substr(containsAt) === "@myseneca.ca")
+		{
+			return true;
+		}
+	}
+	return false;
+};
+
+/**
+ * Given a string `name`, return a formatted Seneca email address for
+ * this person. NOTE: the email doesn't need to be real/valid/active.
+ */
+exports.formatSenecaEmail = function(name) {
+	return name + "@myseneca.ca";
+};
