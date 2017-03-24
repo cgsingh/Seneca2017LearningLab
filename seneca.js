def59105b683e5b51eb570eb20ece5a7bdd29f86
cgsingh <cgsingh@myseneca.ca>
2017-03-24 10:06:29 -0400

    yet another fix, hopefully the last

--- seneca.js
+++ seneca.js
@@ -5,7 +5,7 @@
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
-exports.isValidEmail = function(email) {
+exports.isValidEmail = function (email) {
   const containsAt = email.indexOf('@');
   if (containsAt !== -1 && containsAt !== 0) { // must exist, but must not be first char, obviously
     if (email.substr(containsAt) === '@myseneca.ca') {
@@ -19,6 +19,6 @@ exports.isValidEmail = function(email) {
  * Given a string `name`, return a formatted Seneca email address for
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
-exports.formatSenecaEmail = function(name) {
-  return name + '@myseneca.ca';
+exports.formatSenecaEmail = function (name) {
+  return name.concat('@myseneca.ca');
 };
